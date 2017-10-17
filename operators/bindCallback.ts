e755ad7513d1fa3c1251316411b36243db4a492d
Noah Sun <934937054@qq.com>
2017-10-17 12:31:39 +0800

    bindCallback

--- operators/bindCallback.ts
+++ operators/bindCallback.ts
@@ -2,13 +2,14 @@ import { Observable } from 'rxjs';
 import * as $ from 'jquery';
 
 import { ApiConfig } from '../api/config';
+
 const getAsObservable = Observable.bindCallback($.get);
 const result = getAsObservable('https://cdn.bootcss.com/jquery/3.2.1/core.js');
 result.subscribe({
-    next: (x) => {
+    next: x => {
         console.log(x);
     },
-    error: (err) => {
+    error: err => {
         console.log(err);
     },
 });
